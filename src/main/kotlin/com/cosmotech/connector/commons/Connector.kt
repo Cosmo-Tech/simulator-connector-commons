e1488c8a41a42745d51b2f9aa129dac15809876b
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2020-12-18 15:22:27 +0100

    Add constructSimulatorData() and process() to Connector Interface

--- src/main/kotlin/com/cosmotech/connector/commons/Connector.kt
+++ src/main/kotlin/com/cosmotech/connector/commons/Connector.kt
@@ -1,12 +1,27 @@
 package com.cosmotech.connector.commons
 
+import com.cosmotech.connector.commons.pojo.CsvData
+
 /**
  * Cosmo Solution Connector interface
  */
 interface Connector<T> {
+
     /**
      * Build a client to interact with the targeted data storage
      * @return the client
      */
     fun buildClient(): T
+
+    /**
+     * Construct data that will be transferred to simulator
+     * @return a list of CsvData containing
+     */
+    fun constructSimulatorData(client:T): List<CsvData>
+
+    /**
+     * Retrieve, process and export data
+     */
+    fun process()
+
 }
