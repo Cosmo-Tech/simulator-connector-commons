49c0d300b6141cc989fb36cb3f18902650116e15
Jérémy Reynard <jeremy.reynard@cosmotech.com>
2021-01-12 12:02:45 +0100

    Return a list of generic OUTPUT_TYPE for the method constructSimulatorData

--- src/main/kotlin/com/cosmotech/connector/commons/Connector.kt
+++ src/main/kotlin/com/cosmotech/connector/commons/Connector.kt
@@ -5,19 +5,19 @@ import com.cosmotech.connector.commons.pojo.CsvData
 /**
  * Cosmo Solution Connector interface
  */
-interface Connector<T> {
+interface Connector<CLIENT_TYPE,OUTPUT_TYPE> {
 
     /**
      * Build a client to interact with the targeted data storage
      * @return the client
      */
-    fun buildClient(): T
+    fun buildClient(): CLIENT_TYPE
 
     /**
      * Construct data that will be transferred to simulator
      * @return a list of CsvData containing
      */
-    fun constructSimulatorData(client:T): List<CsvData>
+    fun constructSimulatorData(client:CLIENT_TYPE): List<OUTPUT_TYPE>
 
     /**
      * Retrieve, process and export data
